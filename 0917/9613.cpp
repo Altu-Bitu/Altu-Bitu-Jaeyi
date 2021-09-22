b3269390a6defa782a51f53af6d2f95198f8f3e3
Jaeyi Sung <47311476+Jaeyihello@users.noreply.github.com>
2021-09-22 20:45:26 +0900

    Update 9613.cpp

--- 0917/9613.cpp
+++ 0917/9613.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+vector <int> testcase;
+
+//GCD 구하기 = 유클리드 호제법
+int gcdRecursion(int a, int b) {
+
+    if (a%b == 0) //b가 0이면 a가 최대공약수
+        return b;
+
+    //a%b구한 후 b와 자리 바꾸어서 호출
+    return gcdRecursion(b, a % b);
+}
+
+int main() {
+
+    int n;
+
+    //입력
+    cin >> n ;
+
+    //각 testcase 별 입력-연산-출력
+    // 
+    //input
+    for (int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+        testcase.assign(t, 0);
+
+        for (int j = 0; j < t; j++) {
+            cin >> testcase[j];
+        }
+
+        long long sum = 0;
+        for (int j = 0; j < t; j++) {
+            for (int k = j + 1; k < t; k++) {
+                sum += gcdRecursion(testcase[j], testcase[k]);
+            }
+        }
+
+        cout << sum << '\n';
+
+    }
+
+    return 0;
+}
